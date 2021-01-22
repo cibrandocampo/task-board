490839ca63ce402dbb9ed47a96cfcd183e4d3654
Cibran Docampo Piñeiro <cdocampo@grupo-cps.com>
2021-01-22 13:49:21 +0100

    V 1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lista Tareas
+
+This is a small project developed under Vue.js that allow to the user to have a Front 100% web Taskboard
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app" class="container">
+    <div class="jumbotron">
+      <titulo :titulo="titulo"></titulo>
+      <nueva-tarea :tareas="tareas" 
+                   :actualizarContador="actualizarContador"></nueva-tarea>
+      <lista-tareas :tareas="tareas"></lista-tareas>
+    </div>
+  </div>
+</template>
+
+<script>
+import Titulo from './TituloComponent.vue'
+import NuevaTarea from './NuevaTareaComponent.vue'
+import ListaTareas from './ListaTareasComponent.vue'
+
+export default {
+  components: {
+    Titulo,
+    NuevaTarea,
+    ListaTareas
+  },
+  data(){
+    return {
+      titulo : 'Lista de Tareas',
+      tareas: [
+        {
+          texto: 'Aprender Vue.js',
+          terminada: false
+        },
+        {
+          texto: 'Aprender Angular 2',
+          terminada: false
+        },
+        {
+          texto: 'Aprender Ionic 2',
+          terminada: false
+        }
+      ]
+    }
+  },
+  methods: {
+    actualizarContador(){
+      this.numTareas++;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
